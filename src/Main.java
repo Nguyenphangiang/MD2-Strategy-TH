ce115b4dc2c933afcc225f9ec23f95328d0f3785
Nguyenphangiang <phancruel@gmail.com>
2022-03-09 10:34:40 +0700

    up

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        SortedList sortedList = new SortedList();
+        sortedList.add("Java");
+        sortedList.add("PHP");
+        sortedList.add("Python");
+        sortedList.add("C#");
+
+        sortedList.setSortStrategy(new QuickSort());
+        sortedList.sort();
+
+        sortedList.setSortStrategy(new MergeSort());
+        sortedList.sort();
+    }
+}
